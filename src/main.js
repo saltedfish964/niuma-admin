feb5acb183a6dcbcca7b3a71b79f6f978797960c
SaltedFish964 <saltedfish964@outlook.com>
2025-02-15 17:24:28 +0800

    添加 ant-design-vue 组件库

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue';
 import router from '@src/router';
 import store from '@src/store';
+import { registerAntdComponents } from '@src/utils/antd-components-register';
 import App from '@src/App.vue';
+import 'ant-design-vue/dist/reset.css';
 
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
+
+registerAntdComponents(app);
 
 app.mount('#app');

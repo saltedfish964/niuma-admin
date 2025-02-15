4bf471b5a52eb7ef79c7f49617dbf7e382480c01
SaltedFish964 <saltedfish964@outlook.com>
2025-02-15 17:03:29 +0800

    添加 pinia

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue';
-import Router from '@src/router';
+import router from '@src/router';
+import store from '@src/store';
 import App from '@src/App.vue';
 
 const app = createApp(App);
-app.use(Router);
+
+app.use(store);
+app.use(router);
 
 app.mount('#app');
---
+++ src/store/modules/user.js
@@ -0,0 +1,10 @@
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+
+export const useUserStore = defineStore('user', () => {
+  const userInfo = ref({
+    name: 'NiuMa',
+  });
+
+  return { userInfo };
+});

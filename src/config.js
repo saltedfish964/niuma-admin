0a26fb1289667af5db176605f859542383721899
SaltedFish964 <saltedfish964@outlook.com>
2025-02-17 16:57:38 +0800

    更新 src/config.js iconsList 图标配置

--- src/config.js
+++ src/config.js
@@ -1,13 +1,6 @@
 export default {
   iconsList: [
-    'material-symbols:10k',
-    'material-symbols:10k-outline',
-    'ant-design:account-book-filled',
-    'typcn:bell',
-    'dashicons:admin-appearance',
-    'solar:adhesive-plaster-2-linear',
-    'ant-design:loading-3-quarters-outlined',
-    'flat-color-icons:binoculars',
-    'flat-color-icons:automatic',
+    'ant-design:menu-unfold-outlined',
+    'ant-design:menu-fold-outlined',
   ],
 };

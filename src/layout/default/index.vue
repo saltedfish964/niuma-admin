836939c929f635a9df452a6bad6d843c4462c26c
SaltedFish964 <saltedfish964@outlook.com>
2025-02-17 16:58:29 +0800

    更新 layout/default 新增菜单折叠

--- src/layout/default/index.vue
+++ src/layout/default/index.vue
@@ -1,22 +1,26 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, h } from 'vue';
 import VTabs from '@src/components/tabs/tabs.vue';
+import VIcon from '@src/components/icon/icon.vue';
 
 const items = ref([
   {
     key: 'mail',
     label: 'Navigation One',
     title: 'Navigation One',
+    icon: () => h(VIcon, { name: 'material-symbols-icon-10k-outline' }),
   },
   {
     key: 'app',
     label: 'Navigation Two',
     title: 'Navigation Two',
+    icon: () => h(VIcon, { name: 'custom-icon-vite' }),
   },
   {
     key: 'sub1',
     label: 'Navigation Three - Submenu',
     title: 'Navigation Three - Submenu',
+    icon: h(VIcon, { name: 'custom-icon-vue' }),
     children: [
       {
         type: 'group',
@@ -58,6 +62,7 @@ const items = ref([
   //   title: 'Navigation Four - Link',
   // },
 ]);
+const collapsed = ref(false);
 
 const activeTabKey = ref('home');
 const tabs = ref([
@@ -72,6 +77,10 @@ for (let i = 0; i < 10; i++) {
       .slice(3, Math.random() * 20 + 3)}`,
   });
 }
+
+function toggleMenuHandler() {
+  collapsed.value = !collapsed.value;
+}
 </script>
 
 <template>
@@ -79,21 +88,56 @@ for (let i = 0; i < 10; i++) {
     <div class="menu">
       <div class="main-menu">
         <div class="main-menu-logo">Logo</div>
-        <div class="main-menu-content">
+        <div class="main-menu-content mini-scroll-white">
           <div class="main-menu-item" v-for="item in 20" :key="item">
             <div class="main-menu-item-content"></div>
           </div>
         </div>
       </div>
-      <div class="secondary-menu">
-        <div class="secondary-menu-title">NiuMa Admin</div>
-        <div class="secondary-menu-content">
-          <a-menu mode="inline" :items="items"></a-menu>
+      <div
+        class="secondary-menu"
+        :style="{ width: collapsed ? '81px' : '240px' }"
+      >
+        <div class="secondary-menu-title">
+          <transition name="slide-up">
+            <div v-if="!collapsed" class="secondary-menu-title-text">
+              NiuMa Admin
+            </div>
+            <div
+              v-else
+              class="collapsed-btn-container"
+              @click="toggleMenuHandler"
+            >
+              <v-icon
+                name="ant-design-icon-menu-unfold-outlined"
+                size="24px"
+              ></v-icon>
+            </div>
+          </transition>
+        </div>
+        <div class="secondary-menu-content mini-scroll-black">
+          <a-menu
+            mode="inline"
+            :items="items"
+            :inline-collapsed="collapsed"
+          ></a-menu>
+        </div>
+        <div class="secondary-menu-footer">
+          <div class="collapsed-btn-container" @click="toggleMenuHandler">
+            <v-icon
+              :name="
+                collapsed
+                  ? 'ant-design-icon-menu-unfold-outlined'
+                  : 'ant-design-icon-menu-fold-outlined'
+              "
+              size="24px"
+            ></v-icon>
+          </div>
         </div>
       </div>
     </div>
     <div class="content">
-      <div class="content-header">Header</div>
+      <div class="content-header"></div>
       <div class="content-tabs">
         <div class="content-tabs-container">
           <v-tabs
@@ -124,8 +168,7 @@ for (let i = 0; i < 10; i++) {
   display: flex;
   flex-direction: column;
   flex: none;
-  /* width: 60px; */
-  background: #24272e;
+  background: #031529;
 }
 
 .main-menu-logo {
@@ -141,25 +184,6 @@ for (let i = 0; i < 10; i++) {
   flex: 1;
   overflow: auto;
 }
-/* 滚动条整体样式 */
-.main-menu-content::-webkit-scrollbar {
-  width: 4px; /* 竖向滚动条宽度 */
-  height: 4px; /* 横向滚动条高度 */
-  background-color: transparent; /* 轨道背景透明 */
-}
-
-/* 滚动条滑块 */
-.main-menu-content::-webkit-scrollbar-thumb {
-  background-color: #ffffff; /* 半透明灰色 */
-  border-radius: 2px; /* 圆角 */
-  border: 1px solid transparent; /* 透明边框 */
-  background-clip: content-box; /* 限制背景作用区域 */
-}
-
-/* 滑块悬停效果 */
-.main-menu-content::-webkit-scrollbar-thumb:hover {
-  background-color: #f5f5f5;
-}
 .main-menu-item {
   width: 60px;
   height: 60px;
@@ -169,13 +193,15 @@ for (let i = 0; i < 10; i++) {
   width: 100%;
   height: 100%;
   border-radius: 4px;
-  background: #3e74fd;
+  background: #5271fb;
 }
 .secondary-menu {
   flex: none;
   display: flex;
   flex-direction: column;
   border-right: 1px solid var(--border-color);
+  width: 240px;
+  transition: all 0.3s ease;
 }
 .secondary-menu-title {
   flex: none;
@@ -186,13 +212,40 @@ for (let i = 0; i < 10; i++) {
   font-size: 20px;
   font-weight: bold;
   border-bottom: 1px solid var(--border-color);
+  overflow: hidden;
+}
+.secondary-menu-title-text {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.collapsed-btn-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 38px;
+  height: 38px;
+  border-radius: 4px;
+  transition: all 0.3s ease;
+}
+.collapsed-btn-container:hover {
+  background: #f0f0f0;
+  cursor: pointer;
 }
 .secondary-menu-content {
   flex: 1;
   overflow: auto;
 }
-.secondary-menu-content .ant-menu-light.ant-menu-root.ant-menu-inline {
+.secondary-menu-content .ant-menu-light.ant-menu-root.ant-menu-inline,
+.secondary-menu-content :deep(.ant-menu-light.ant-menu-root.ant-menu-vertical) {
   border-inline-end: none;
+}
+.secondary-menu-footer {
+  height: var(--header-height);
+  border-top: 1px solid var(--border-color);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .content {
   flex: 1;
@@ -220,5 +273,27 @@ for (let i = 0; i < 10; i++) {
   flex: 1;
   overflow: auto;
   background: var(--border-color);
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  display: none;
+}
+
+.slide-up-enter-active,
+.slide-up-leave-active {
+  transition: all 0.3s ease;
+  overflow: hidden;
+}
+
+.slide-up-enter-from,
+.slide-up-leave-to {
+  opacity: 0;
+  font-size: 0;
 }
 </style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,3 +1,11 @@
+<script setup>
+import VIcon from '@src/components/icon/icon.vue';
+</script>
+
 <template>
-  <div>Home</div>
+  <div>
+    <div>Home</div>
+    <v-icon name="custom-icon-vue"></v-icon>
+    <v-icon name="custom-icon-vite"></v-icon>
+  </div>
 </template>

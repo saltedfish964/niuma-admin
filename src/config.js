910b9365ddff5e99efc6aedcdbe2d71ed8ee6f02
SaltedFish964 <saltedfish964@outlook.com>
2025-02-19 00:04:02 +0800

    新增 若干图标 icon

--- src/config.js
+++ src/config.js
@@ -3,5 +3,8 @@ export default {
     'ant-design:menu-unfold-outlined',
     'ant-design:menu-fold-outlined',
     'flat-color-icons:home',
+    'material-symbols:iframe',
+    'ic:baseline-insert-emoticon',
+    'line-md:link',
   ],
 };

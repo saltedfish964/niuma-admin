35d3ebe9e05a942e1ed65d24f0336e4eddc02004
SaltedFish964 <saltedfish964@outlook.com>
2025-02-22 00:06:09 +0800

    新增 ant-design-vue 配置为 zhCN

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 import { createApp } from 'vue';
-import VxeUITable from 'vxe-table';
+import dayjs from 'dayjs';
+
 import router from '@src/router';
 import store from '@src/store';
 import { registerAntdComponents } from '@src/utils/antd-components-register';
 import App from '@src/App.vue';
+import 'dayjs/locale/zh-cn';
 import 'ant-design-vue/dist/reset.css';
-import 'vxe-table/lib/style.css';
 import '@src/styles/root.css';
 import '@src/styles/scroll.css';
 
+dayjs.locale('zh-cn');
+
 const app = createApp(App);
 
-app.use(VxeUITable);
 app.use(store);
 app.use(router);
 

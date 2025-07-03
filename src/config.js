b5218112ee1ad6281582ec7ebe28ad565d0e44d9
SaltedFish964 <saltedfish964@outlook.com>
2025-07-03 17:09:46 +0800

    更新 config.js 文件增加 menu 配置

--- src/config.js
+++ src/config.js
@@ -1,4 +1,72 @@
+const MENU_KEYS = {
+  INDEX: 'index',
+  OVERVIEW: 'overview',
+  ICON: 'icon',
+};
+
+/**
+ * @typedef {Object} MenuItemMeta
+ * @property {string} icon - 菜单图标
+ * @property {string} layout - 路由布局
+ * @property {string} component - 路由组件
+ * @property {boolean} hide - 是否隐藏
+ */
+/**
+ * @typedef {Object} MenuItem
+ * @property {string} key - 菜单 key
+ * @property {string} title - 菜单标题
+ * @property {string} [path] - 路由路径
+ * @property {MenuItemMeta} meta - 菜单 meta
+ * @property {MenuItem[]} [children] - 子菜单
+ */
 export default {
+  /**
+   * 菜单
+   * @type {MenuItem[]}
+   */
+  menu: [
+    {
+      key: MENU_KEYS.INDEX,
+      title: '首页',
+      meta: {
+        icon: 'ant-design-icon-home-filled',
+        // hide: false,
+      },
+      children: [
+        {
+          key: MENU_KEYS.OVERVIEW,
+          title: '概览',
+          path: '/overview',
+          meta: {
+            icon: 'solar-icon-chart-outline',
+            layout: 'main',
+            component: 'overview',
+          },
+        },
+        {
+          key: MENU_KEYS.ICON,
+          title: '图标',
+          path: '/icon',
+          meta: {
+            icon: 'ic-icon-baseline-insert-emoticon',
+            layout: 'main',
+            component: 'icon',
+          },
+        },
+      ],
+    },
+    {
+      key: 'test',
+      title: '测试',
+      path: '/bank-test',
+      meta: {
+        icon: 'ic-icon-baseline-insert-emoticon',
+        layout: 'bank',
+        hide: true, // 在菜单中隐藏
+        component: 'icon',
+      },
+    },
+  ],
   iconsList: [
     'ant-design:menu-unfold-outlined',
     'ant-design:menu-fold-outlined',

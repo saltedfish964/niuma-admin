65ca0a895a8162dbc297ebd628b7705202382b10
SaltedFish964 <saltedfish964@outlook.com>
2025-07-03 17:08:07 +0800

    删除 router/modules 中的无用模块

--- src/router/modules/big-screen.js
+++
@@ -1,7 +0,0 @@
-export default [
-  {
-    path: '/big-screen',
-    name: 'big-screen',
-    component: () => import('@src/views/big-screen/index.vue'),
-  },
-];
--- src/router/modules/not-found.js
+++
@@ -1,7 +0,0 @@
-export default [
-  {
-    name: 'not-found',
-    path: '/:pathMatch(.*)*',
-    component: () => import('@src/views/not-found/index.vue'),
-  },
-];

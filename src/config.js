37bf083cc44981570399812134fc136c0432eebb
SaltedFish964 <saltedfish964@outlook.com>
2025-07-12 23:35:30 +0800

    更新 config.js 配置文件调整

--- src/config.js
+++ src/config.js
@@ -19,65 +19,73 @@ const MENU_KEYS = {
  * @property {MenuItemMeta} meta - 菜单 meta
  * @property {MenuItem[]} [children] - 子菜单
  */
+/**
+ * 菜单
+ * @type {MenuItem[]}
+ */
+const menu = [
+  {
+    key: MENU_KEYS.INDEX,
+    title: '首页',
+    meta: {
+      icon: 'ant-design-icon-home-filled',
+      // hide: false,
+    },
+    children: [
+      {
+        key: MENU_KEYS.OVERVIEW,
+        title: '概览',
+        path: '/overview',
+        meta: {
+          icon: 'solar-icon-chart-outline',
+          layout: 'main',
+          component: 'overview',
+        },
+      },
+      {
+        key: MENU_KEYS.ICON,
+        title: '图标',
+        path: '/icon',
+        meta: {
+          icon: 'ic-icon-baseline-insert-emoticon',
+          layout: 'main',
+          component: 'icon',
+        },
+      },
+    ],
+  },
+  {
+    key: 'test',
+    title: '测试',
+    path: '/bank-test',
+    meta: {
+      icon: 'ic-icon-baseline-insert-emoticon',
+      layout: 'bank',
+      hide: true, // 在菜单中隐藏
+      component: 'icon',
+    },
+  },
+];
+
+/**
+ * 图标
+ * @type {string[]}
+ */
+const iconsList = [
+  'ant-design:menu-unfold-outlined',
+  'ant-design:menu-fold-outlined',
+  'ant-design:home-filled',
+  'flat-color-icons:home',
+  'material-symbols:iframe',
+  'ic:baseline-insert-emoticon',
+  'line-md:link',
+  'solar:chart-outline',
+  'icon-park-outline:workbench',
+  'ant-design:table-outlined',
+  'carbon:screen',
+];
+
 export default {
-  /**
-   * 菜单
-   * @type {MenuItem[]}
-   */
-  menu: [
-    {
-      key: MENU_KEYS.INDEX,
-      title: '首页',
-      meta: {
-        icon: 'ant-design-icon-home-filled',
-        // hide: false,
-      },
-      children: [
-        {
-          key: MENU_KEYS.OVERVIEW,
-          title: '概览',
-          path: '/overview',
-          meta: {
-            icon: 'solar-icon-chart-outline',
-            layout: 'main',
-            component: 'overview',
-          },
-        },
-        {
-          key: MENU_KEYS.ICON,
-          title: '图标',
-          path: '/icon',
-          meta: {
-            icon: 'ic-icon-baseline-insert-emoticon',
-            layout: 'main',
-            component: 'icon',
-          },
-        },
-      ],
-    },
-    {
-      key: 'test',
-      title: '测试',
-      path: '/bank-test',
-      meta: {
-        icon: 'ic-icon-baseline-insert-emoticon',
-        layout: 'bank',
-        hide: true, // 在菜单中隐藏
-        component: 'icon',
-      },
-    },
-  ],
-  iconsList: [
-    'ant-design:menu-unfold-outlined',
-    'ant-design:menu-fold-outlined',
-    'ant-design:home-filled',
-    'flat-color-icons:home',
-    'material-symbols:iframe',
-    'ic:baseline-insert-emoticon',
-    'line-md:link',
-    'solar:chart-outline',
-    'icon-park-outline:workbench',
-    'ant-design:table-outlined',
-    'carbon:screen',
-  ],
+  menu,
+  iconsList,
 };

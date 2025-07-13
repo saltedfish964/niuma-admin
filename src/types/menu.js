f0b9d7e018ff1f06f2ba03f00bb341007bffada3
SaltedFish964 <saltedfish964@outlook.com>
2025-07-13 15:43:11 +0800

    新增 types/* 配置目录管理 jsdoc

---
+++ src/types/menu.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {Object} MenuItemMeta
+ * @property {string} icon - 菜单图标
+ * @property {string} layout - 路由布局
+ * @property {string} component - 路由组件
+ * @property {boolean} hide - 是否隐藏菜单
+ * @property {boolean} unloadRouter - 不加载路由
+ */
+
+/**
+ * @typedef {Object} MenuItem
+ * @property {string} key - 菜单 key
+ * @property {string} title - 菜单标题
+ * @property {string} [path] - 路由路径
+ * @property {MenuItemMeta} meta - 菜单 meta
+ * @property {MenuItem[]} [children] - 子菜单
+ */

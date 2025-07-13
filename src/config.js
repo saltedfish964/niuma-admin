9fee6172f1ccc38ed18f3d5f4ee10a3d50a1e7d8
SaltedFish964 <saltedfish964@outlook.com>
2025-07-13 15:13:18 +0800

    更新 config.js 的 menu 配置增加 unloadRouter 选项

--- src/config.js
+++ src/config.js
@@ -9,7 +9,8 @@ const MENU_KEYS = {
  * @property {string} icon - 菜单图标
  * @property {string} layout - 路由布局
  * @property {string} component - 路由组件
- * @property {boolean} hide - 是否隐藏
+ * @property {boolean} hide - 是否隐藏菜单
+ * @property {boolean} unloadRouter - 不加载路由
  */
 /**
  * @typedef {Object} MenuItem

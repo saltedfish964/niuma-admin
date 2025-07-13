81efcb46c9e49c7a27189f481ea047628ad2d433
SaltedFish964 <saltedfish964@outlook.com>
2025-07-14 00:23:33 +0800

    更新 router 增加 beforeEach 处理

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,19 @@
 import { createWebHashHistory, createRouter } from 'vue-router';
 import BasicRoutes from '@src/router/modules/basic';
+import { useLayoutStore } from '@src/store/modules/layout';
 
 const routes = [...BasicRoutes];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const layoutStore = useLayoutStore();
+  layoutStore.updateTabByRouteFullPath(to.fullPath);
+  layoutStore.updateMenuActiveByRouteFullPath(to.fullPath);
+  next();
 });
 
 export default router;

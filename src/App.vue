36149c44a84164d16a44e14785870c2e1a4bf820
SaltedFish964 <saltedfish964@outlook.com>
2025-07-17 02:55:22 +0800

    更新 App.vue 在浏览器刷新之前保存当前路由地址用于刷新后恢复

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script setup>
 import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import { useRouter } from 'vue-router';
+import { useBeforeUnload } from '@src/composables/useBeforeUnload';
+
+const router = useRouter();
+
+useBeforeUnload(() => {
+  const currentRouteFullPath = router.currentRoute.value.fullPath;
+  sessionStorage.setItem('currentRouteFullPath', currentRouteFullPath);
+});
 </script>
 
 <template>

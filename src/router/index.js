db83f8255ca8a63934ed2a898d2bbdaa26ea124b
SaltedFish964 <saltedfish964@outlook.com>
2025-07-17 03:01:37 +0800

    更新 router/index 路由前置守卫增加 token 和动态路由处理

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createWebHashHistory, createRouter } from 'vue-router';
 import BasicRoutes from '@src/router/modules/basic';
 import { useLayoutStore } from '@src/store/modules/layout';
+import { useUserStore } from '@src/store/modules/user';
+import { getMenu } from '@src/api/menu';
+import { mergeNewConfigToMenuConfig } from '@src/utils/config';
+import { addRoutes } from '@src/router/dynamicRoutes';
 
 const routes = [...BasicRoutes];
 
@@ -9,12 +13,40 @@ const router = createRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   const layoutStore = useLayoutStore();
-  const meta = to.meta;
-  layoutStore.updateTabByKey(meta.key);
-  layoutStore.updateMenuActiveByKey(meta.key);
-  next();
+  const userStore = useUserStore();
+
+  const token = userStore.getToken();
+
+  if (!token) {
+    if (to.path === '/login') {
+      next();
+    } else {
+      next('/login');
+    }
+  } else {
+    if (layoutStore.menuList.length === 0) {
+      const res = await getMenu();
+      const config = mergeNewConfigToMenuConfig(res);
+      layoutStore.generateMenuList(config);
+      addRoutes(config);
+      const path = sessionStorage.getItem('currentRouteFullPath');
+      if (path) {
+        next(path);
+        sessionStorage.removeItem('currentRouteFullPath');
+      } else {
+        next();
+      }
+    } else if (to.path === '/login') {
+      next(from);
+    } else {
+      const meta = to.meta;
+      layoutStore.updateTabByKey(meta.key);
+      layoutStore.updateMenuActiveByKey(meta.key);
+      next();
+    }
+  }
 });
 
 export default router;

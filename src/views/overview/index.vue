6a5582f76c7de9e4330a056eef788c6077029c6c
SaltedFish964 <saltedfish964@outlook.com>
2025-08-04 13:23:23 +0800

    更新 views/overview 概览页增加描述和图片

--- src/views/overview/index.vue
+++ src/views/overview/index.vue
@@ -1,9 +1,46 @@
 <script setup>
+import WorkingSvg from './working-svg.vue';
+
 defineOptions({
   name: 'Overview',
 });
 </script>
 
 <template>
-  <div>概览</div>
+  <div class="page-container">
+    <div class="body">
+      <div class="content">
+        <h1>开启牛马的一天</h1>
+        <div class="img-container">
+          <working-svg class="img"></working-svg>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.page-container {
+  padding: 8px;
+  height: 100%;
+}
+.body {
+  height: 100%;
+  border-radius: 4px;
+  background: var(--nm-color-bg-container);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.content h1 {
+  text-align: center;
+}
+.img-container {
+  display: flex;
+  justify-content: center;
+}
+.img-container .img {
+  height: 100%;
+  color: var(--nm-color-text);
+}
+</style>
